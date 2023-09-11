a4793051cfbd9f54a29487abeb56a8696b1e2d06
kentcliu <132862709+kentcliu@users.noreply.github.com>
2023-09-11 15:58:33 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<script setup>
+import todoInfo from "@/components/headers/message.vue"
+import todoItem from "@/components/todo/checklist.vue"
+import { v4 as uuidv4 } from "uuid"
+import { ref, reactive, onBeforeMount } from "vue"
+
+const todo = ref("")
+const todos = reactive([])
+
+const addtodolist = () => {
+  if (todo.value !== "") {
+    console.log(todo.value)
+    const item = {
+      id: uuidv4(),
+      title: todo.value,
+      completed: false,
+    }
+    todos.unshift(item)
+    console.log(todos)
+    todo.value = ""
+  }
+}
+
+const removeItem = (id) => {
+  const index = todos.findIndex((todo) => {
+    return todo.id === id
+  })
+
+  todos.splice(index, 1)
+}
+
+
+</script>
+
+<template>
+    <main class="container mx-auto">
+        <header class="m-2">
+            <h1 class="text-6xl font-thin select-none">TO DO LIST!</h1>
+            <div class="font-semibold select-none text-neutral-600">Daily check To Do List(Do something)</div>
+        </header>
+        <form class="px-10 py-12 bg-white shadow-sm">
+            <section class="flex">
+                <input type="text" placeholder="日常點檢事件，做點事吧..." class="w-full text-2xl focus:outline-none input-lg input input-bordered"
+                v-model="todo" />
+                <button class="text-xl btn-lg btn btn-neutral" @click.prevent="addtodolist" >Add(新增)</button>
+            </section>
+        </form>
+
+        <section class="px-8 py-2 mt-6 bg-white">
+            <header>
+            <todoInfo :todos="todos" />
+            </header>
+
+            <ul>
+            <todoItem
+                @remove-item="removeItem"
+                v-for="d in todos"
+                :todo="d"
+            />
+            </ul>
+        </section>
+
+    </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/headers/message.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { computed } from "vue"
+
+const props = defineProps({
+  todos: {
+    type: Array,
+    required: true,
+    default: [],
+  },
+})
+
+</script>
